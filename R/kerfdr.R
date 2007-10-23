f9451e710d216002b7df16a0c8742f3f6ac32eef
M Guedj <mickael.guedj@gmail.com>
2007-10-23 00:00:00 +0000

    version 1.0.1

--- R/kerfdr.R
+++ R/kerfdr.R
@@ -1,16 +1,62 @@
-kerfdr = function (
-  pv, x = NULL, f0 = NULL, localfdr = NULL, pi1 ="storey",
-  lambda=0.5, bw = "sj-dpi", adjust = 1.0,
-  trans = c("probit", "log", "none"),
-  kernel = c("gaussian","epanechnikov", "rectangular", "triangular", "biweight","cosine", "optcosine"),
-  pvMin = 0.0, pvMax = 1.0, verbose = FALSE, plot = TRUE)  {
+# Rd
+# description >> This function computes local fdr values by using a two-components mixture model with a semi-parametric density estimation. The code is freely inspired from the \link[stats]{density} function. For a simple use, we recommand the default setting (most parameters are optional). 
+# argument
+# item >> pv >> the vector of raw p-values.
+# item >> x >> a transformation of \code{pv}. It can be given by the user or (if \code{NULL}) computed via the \code{trans} parameter    
+# item >> trans >> the transformation to apply on \code{pv} to produce \code{x}: \code{"probit"} (by default) returns \code{qnorm(pv)} and \code{"log"} returns \code{log10(pv)}. 
+# item >> f0 >> the sample density under the null hypothesis. Can be specified by the user. If \code{NULL} (by default) the density under H0 is determined according to \code{trans}: if \code{trans = "probit"} then \code{f0} is a standard Gaussian distribution; if \code{trans = "log"} then \code{f0} is a standard Exponential distribution; if \code{trans = "none"} then \code{f0} is a standard Uniform distribution
+# item >> localfdr >> values to initiate the iterative algorithm. If \code{NULL} (by default) initial values are then sampled in a Uniform distribution [0,1]
+# item >> pi1 >> a priori proportion of alternative hypothesis or a method (string) to compute it; by default it uses the method proposed by Storey and Tibshirani (2003).
+# item >> lambda >> p-value threshold for the Storey's calculation of \code{pi1} (0.5 by default). See \link[qvalue]{qvalue} for more details. 
+# item >> bw >> a bandwidth value or a method to determine it among \code{"nrd0", "nrd", "ucv", "bcv", "sj-ste", "sj-dpi"}. See \link[stats]{bandwidth} for more details. 
+# item >> kernel >> the kernel used (string) among \code{"gaussian"} (by default), \code{"epanechnikov", "rectangular", "triangular", "biweight","cosine"}. For more details on kernels: \url{http://stat.genopole.cnrs.fr/sg/software/kerfdr/kernels}
+# item >> truncat >> an interval on p-values to deal with truncated distributions such as those obtained with Monte-Carlo simulations.   
+# item >> plot >> if \code{TRUE}, it returns graphics of local fdr estimations. Some plots are inspired from \link[qvalue]{qvalue}. 
+# item >> cuts >> vector of significance values to use in \code{summary} (see below)
+# value >> A list of parameters (\code{pv, x, pi1, bw, f0} ...) and the following results: 
+# item >> f >> the observed mixture density
+# item >> f1 >> the estimated density under H1
+# item >> localfdr >> the local fdr values resulting from the algorithm 
+# item >> summary >> a summary table comparing the number of significant calls for the raw p-values, Bonferroni and Benjamini-Hochberg corrections and for the calculated local fdr, using a set of cutoffs given by \code{cuts}
+# author >> M Guedj, A Celisse, S Robin, G Nuel
+# keyword >> nonparametric
+# references >> \url{http://stat.genopole.cnrs.fr/sg/software/kerfdr}, Robin et al (2007), Strimmer (2008), Guedj et al (under review)
+# examples >> # Example 1: kerfdr with different plots >> generating a vector of p-values with pi0 = 80%
+# examples >> n = 10000
+# examples >> pi0 = 0.8
+# examples >> # plot in a probit scale (default)
+# examples >> pv = 1-pnorm(c(rnorm(n*pi0), rnorm(n*(1-pi0), 4)))
+# examples >> res = kerfdr(pv)
+# examples >> res$pi0
+# examples >> res$summary
+# examples >> # plot in a log scale
+# examples >> kerfdr(pv, trans = "log")
+# examples >> # plot in the raw p-values scale
+# examples >> kerfdr(pv, trans = "none")
+# examples >> # Example 2: truncation on a vector of null p-values (resulting local fdr should be 1 for each point)
+# examples >> n = 10000
+# examples >> pv = runif(n)
+# examples >> # truncation on [0.1;0.9] 
+# examples >> pv[which(pv < 0.1)] = 0.1
+# examples >> pv[which(pv > 0.9)] = 0.9
+# examples >> # kerfdr WITHOUT taking the truncation into account (local fdr is hence badly estimated)
+# examples >> kerfdr(pv, trans = "log")
+# examples >> # kerfdr by taking the truncation into account (local fdr is then well estimated)
+# examples >> kerfdr(pv, truncat = c(0.1, 0.9), trans = "log")
+# end
+kerfdr = function(
+  pv, x = NULL, trans = c("probit", "log", "none"), f0 = NULL, localfdr = NULL, pi1 ="storey",
+  lambda=0.5, bw = c("nrd0", "nrd", "ucv", "bcv", "sj-ste", "sj-dpi"),
+  kernel = c("gaussian","epanechnikov", "rectangular", "triangular", "biweight","cosine"),
+  truncat = c(0,1), plot = TRUE, cuts = c(1e-04, 0.001, 0.01, 0.025, 0.05, 0.1))  {
         # general parameters
   n = 512;
+  adjust = 1.0
   give.Rkern = FALSE;
   cut = 3;
   tol = 1e-15;
   maxiter = 2000;
-
+  bw = match.arg(bw)
 	# parse options
 	kernel = match.arg(kernel)
 	trans = match.arg(trans)
@@ -29,12 +75,12 @@ kerfdr = function (
     	x = as.vector(x)
     		
 	# truncature if needed
-  truncinf=(pv<=pvMin);
-  truncsup=(pv>=pvMax);
+  truncinf=(pv<=truncat[1]);
+  truncsup=(pv>=truncat[2]);
   trunc=(!truncinf & !truncsup);
 	nx=sum(trunc);
 	if (nx==0)
-		stop(paste("no p-values in the valid domains: ]",pvMin, " ; ", pvMax, " ["));
+		stop(paste("no p-values in the valid domains: ]",truncat[1], " ; ", truncat[2], " ["));
 	xtrunc=x[trunc];
   
 	# get working index
@@ -77,7 +123,7 @@ kerfdr = function (
 
 	# random initialization of tau = 1.0 - localfdr
 	tau=runif(x);
-	tau[!working]=1-localfdr[!working];
+	tau[!working]=1.0-localfdr[!working];
 
 	# compute working interval		
 	from = min(xtrunc) - cut * bw
@@ -90,8 +136,8 @@ kerfdr = function (
 	up = to + 4 * bw
 
 	# compute truncature conditional probabilities
-  p0inf=pvMin;
-  p0sup=1-pvMax;
+  p0inf=truncat[1];
+  p0sup=1-truncat[2];
   if (pi1>0) {
     p1inf=(sum(truncinf)/length(pv)-pi0*p0inf)/pi1;
     p1sup=(sum(truncsup)/length(pv)-pi0*p0sup)/pi1;
@@ -154,8 +200,6 @@ kerfdr = function (
 		test = max(abs(tau-newtau))
 		if (test < tol) 
 			converged = TRUE
-		if (verbose) 
-			print(paste("iter =",round(iter),"; test =",test))
 		tau = newtau
 		if (sum(tau[trunc])==0.0) {
 		   converged= TRUE;
@@ -171,18 +215,28 @@ kerfdr = function (
 	x[truncinf]=min(xtrunc);
 	x[truncsup]=max(xtrunc);
 
+  bon = pv*length(pv)
+  bon[which(bon > 1)] = 1
+  bh = pv*length(pv)/rank(pv)
+  bh[which(bh > 1)] = 1
+  
+  counts <- sapply(cuts, function(x) c("p-value" = sum(pv <= x), "FWER (Bonf.)"= sum(bon <= x), "FDR (BH)"= sum(bh <= x),"kerfdr" = sum(localfdr <= x)))
+  colnames(counts) <- paste("<", cuts, sep = "")
+
+
 	# results as a density object
     	results = list(pv = pv, x = x, f = pi1*f1+(1-pi1)*f0,
-		f0 = f0, f1 = f1,pi0 = pi0, pi1 = pi1, p0 = p0, p1 = p1, localfdr = localfdr,
+		f0 = f0, f1 = f1,pi0 = pi0, pi1 = pi1, p0 = p0, p1 = p1,
 		bw = bw, iter = iter, call = match.call(), data.name = name,
-		kords = kords)
+		kords = kords, localfdr = localfdr, summary = counts)
 
 	# graphic output
 	if (plot == TRUE)
 	{
+	  layout(matrix(c(1,1, 2, 2,3,4), 3,2, byrow = TRUE))
 		xlab=switch(trans,probit="probit(pv)",log="log10(pv)",none="pv");
 		main=paste("kerfdr(): pi1 = ",round(1000*pi1)/1000," and bw = ",round(1000*bw)/1000);
-		par(mfrow = c(2,1))
+		#par(mfrow = c(2,1))
 		hist(x,freq = FALSE, col = "lightgrey", border= "darkgrey",
 			xlab = xlab, ylab = "density", main = main, ylim=c(0,max(results$f[trunc])),
 			nc=max(20,round(diff(range(xtrunc))/bw)))
@@ -192,7 +246,8 @@ kerfdr = function (
 		lines(xtrunc[I],results$f[trunc][I], col = "black")
 		plot(range(xtrunc),c(0,1.0),t="n",xlab=xlab, ylab="local fdr");
 		points(x,localfdr, cex = 0.5, pch = 20)
-		
+		plot(pv, localfdr,xlab = "p-value", ylab = "local fdr", xlim = c(0,1), ylim = c(0,1), col = "darkgrey", cex = 0.7) 
+		plot(sort(localfdr), 1:length(localfdr),xlab = "local fdr", ylab = "nb of tests", xlim = c(0,1), col = "darkgrey", cex = 0.7) 	
 	}
 	
 	return(results)
